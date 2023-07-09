a0112598c957a194ab437d7edd1d3091dfa96a53
Diego Anjos <diegoanjos@gmail.com>
2023-07-08 21:25:22 -0300

    update Usuario.java

--- src/model/entities/Usuario.java
+++ src/model/entities/Usuario.java
@@ -29,6 +29,54 @@ public class Usuario {
 			this.devolucaoStrategy = new DevolucaoProfessorStrategy();
 		}
 	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public EmprestimoStrategy getEmprestimoStrategy() {
+		return emprestimoStrategy;
+	}
+
+	public ReservaStrategy getReservaStrategy() {
+		return reservaStrategy;
+	}
+
+	public DevolucaoStrategy getDevolucaoStrategy() {
+		return devolucaoStrategy;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setEmprestimoStrategy(EmprestimoStrategy emprestimoStrategy) {
+		this.emprestimoStrategy = emprestimoStrategy;
+	}
+
+	public void setReservaStrategy(ReservaStrategy reservaStrategy) {
+		this.reservaStrategy = reservaStrategy;
+	}
+
+	public void setDevolucaoStrategy(DevolucaoStrategy devolucaoStrategy) {
+		this.devolucaoStrategy = devolucaoStrategy;
+	}
 	
 	
 }
